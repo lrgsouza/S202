4888b79c0b4bea94755c136a12058456e171cccb
Lucas Souza <lucas_souza@outlook.com>
2022-08-27 17:20:54 -0300

    Relatório 03 - MongoDB parte 02

--- db/pokedex.py
+++ db/pokedex.py
@@ -59,3 +59,55 @@ class Pokedex:
                 evolutions.append(evolution['name'])
 
         return evolutions
+
+    ### ======= RELATORIO ======= ###
+    def get_typeByName(self,name: str):
+        #buscando pokemon
+        pokemon = self.getPokemonByName(name)
+        #verificando tipo
+        type = [pokemon['type']]
+        #retorna tipo
+        return type
+
+    def get_MostHeavyPokemon(self):
+        response = self.collection.find({}, {"name": 1,"weight": 1 ,"_id": 0})
+        weight_dict = {}
+        poke_name = 'Not Found'
+        weight = 0.0
+        for pokemon in response:
+            weight_dict[pokemon['name']] = float(pokemon['weight'].split()[0])
+        for pokemon in weight_dict:
+            if weight_dict[pokemon] > weight:
+                weight = weight_dict[pokemon]
+                poke_name = pokemon
+
+        return poke_name, weight
+
+    def get_MostTallPokemon(self):
+        response = self.collection.find({}, {"name": 1,"height": 1 ,"_id": 0})
+        height_dict = {}
+        poke_name = 'Not Found'
+        height = 0.0
+        for pokemon in response:
+            height_dict[pokemon['name']] = float(pokemon['height'].split()[0])
+        for pokemon in height_dict:
+            if height_dict[pokemon] > height:
+                height = height_dict[pokemon]
+                poke_name = pokemon
+
+        return poke_name, height
+
+    def get_PokemonByWeaknesses(self, weakness: str):
+        response = self.collection.find({"weaknesses": {"$all": [weakness]}},{"name": 1, "_id": 0})
+        pokemons = []
+        for pokemon in response:
+            pokemons.append(pokemon['name'])
+        return pokemons
+
+    def get_PokemonSpawChance(self):
+        response = self.collection.find({"spawn_chance": {"$gt": 1.0}},{"name": 1, "spawn_chance": 1, "_id": 0})
+        pokemons_spaw = {}
+        for pokemon in response:
+            pokemons_spaw[pokemon['name']] = pokemon['spawn_chance']
+
+        return pokemons_spaw
--- main.py
+++ main.py
@@ -2,6 +2,33 @@ from db.pokedex import Pokedex
 from helper.WriteAJson import writeAJson
 
 pokedex = Pokedex()
+'''
+Entrega do relatório 3 parte 2.
+Desenvolvimento de 5 metodos na classe Pokedex para pesquisas utilizando MongoDB
+'''
 
+#query para verificar o tipo passando o nome
+name = 'Snorlax'
+type = pokedex.get_typeByName(name)[0]
+print(f'{name} is a {[f for f in type]} Pokemon')
 
+#query para buscar o pokemon mais pesado
+name, weight = pokedex.get_MostHeavyPokemon()
+print(f'Most heavy pokemon: {name} - {weight}Kg')
 
+#query para buscar o pokemon mais alto
+name, height = pokedex.get_MostTallPokemon()
+print(f'Most tall pokemon: {name} - {height}m')
+
+#query para buscar pela fraqueza
+weak = 'Ghost'
+pokemons = pokedex.get_PokemonByWeaknesses(weak)
+print(f'Pokemons thats have {weak} weakness:')
+for i, pokemon in enumerate(pokemons):
+    print(f'{i} - {pokemon}')
+
+#query para buscar pokemons com spaw chance > 1
+pokespaws = pokedex.get_PokemonSpawChance()
+print(f'Pokemons thats have Spaw chance greater than 1:')
+for pokemon in pokespaws:
+    print(f'{pokemon} - Spaw chance = {pokespaws[pokemon]}')
